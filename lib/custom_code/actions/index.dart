9601ded57c42fdcd12e313fdd00127de4842d6b3
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-12-11 18:11:16 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/index.dart
+++ lib/custom_code/actions/index.dart
@@ -1,1 +1,2 @@
 export 'one_signal.dart' show oneSignal;
+export 'lock_orientation.dart' show lockOrientation;
---
+++ lib/custom_code/actions/lock_orientation.dart
@@ -0,0 +1,17 @@
+// Automatic FlutterFlow imports
+import '/backend/backend.dart';
+import '/backend/schema/structs/index.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom actions
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom action code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+import 'package:flutter/services.dart';
+
+Future lockOrientation() async {
+  // Add your function code here!
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+}
